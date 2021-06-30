de868566881642d03739b4807f0c30cc0a437e54
hasankayman <hasankayman@gmail.com>
2021-06-30 09:25:40 -0600

    CucumberRunner.java tag @smoke added

--- src/test/java/com/cybertek/runners/CucumberRunner.java
+++ src/test/java/com/cybertek/runners/CucumberRunner.java
@@ -20,14 +20,14 @@ import org.junit.runner.RunWith;
         //tags = "@login and not @student"
         //tags = "@student"
         //tags = "@login and @sunday and not @admin"
-        tags = "@student or @admin",
+        //tags = "@student or @admin",
         //tags = "@search and @keyword"
         //tags = "@about"
         //tags = "@googleSearch"
         //tags = "@smartbearLogin"
         //tags = "@etsy"
         //tags = "@wikipedia",
-        //tags = "@fail"
+        tags = "@smoke",
         publish = true
 
 
